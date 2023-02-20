8150ae7aceb901fcf0b47c4e709722ca27dedb3b
Rose Khaldeeva <khalrose@mail.ru>
2023-02-20 22:23:11 +0600

    Add ViewControllers

---
+++ Tipsy1.0/ViewControllers/SayCalculateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Tipsy1.0
+//
+//  Created by Rose on 20.02.2023.
+//
+
+import UIKit
+
+class SayCalculateViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+}
+
